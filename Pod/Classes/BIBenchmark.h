198c135c6593419f068021c710a86bbab7d4a928
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2015-02-23 07:21:35 +0100

    Fix compiler warning

--- Pod/Classes/BIBenchmark.h
+++ Pod/Classes/BIBenchmark.h
@@ -5,7 +5,12 @@
 
 extern uint64_t dispatch_benchmark(size_t count, void (^block)(void));
 
-#define BIBench(tag, block) uint64_t took = dispatch_benchmark(1, ^{ block }); BILogDebug(@"%@ - %.2f ms", tag, took/1000000.0)
+#define BIBench(tag, block) \
+    _Pragma("clang diagnostic push") \
+    _Pragma("clang diagnostic ignored \"-Wunused-variable\"") \
+    uint64_t took = dispatch_benchmark(1, ^{ block }); \
+    BILogDebug(@"%@ - %.2f ms", tag, took/1000000.0) \
+    _Pragma("clang diagnostic pop")
 #else
     #define BIBench(tag, block) block
 #endif
--- Pod/Classes/Enums/BIEnumsRegistry.h
+++ Pod/Classes/Enums/BIEnumsRegistry.h
@@ -16,11 +16,10 @@
 #ifdef DEBUG
 #define BIEnumFor(typeName) \
         [BIEnumsRegistry enumFor: @metamacro_stringify(typeName)]; \
-_Pragma("clang diagnostic push") \
+        _Pragma("clang diagnostic push") \
         _Pragma("clang diagnostic ignored \"-Wunused-variable\"") \
         typeName _;\
-_Pragma("clang diagnostic pop") \
-
+        _Pragma("clang diagnostic pop")
 #else
         #define BIEnumFor(typeName) [BIEnumsRegistry enumFor: @metamacro_stringify(typeName)]
     #endif
@@ -29,11 +28,11 @@ _Pragma("clang diagnostic pop") \
 #ifndef BIRegisterSelfAsEnum
 #ifdef DEBUG
 #define BIRegisterSelfAsEnum(typeName) \
-        _Pragma("clang diagnostic push") \
-        _Pragma("clang diagnostic ignored \"-Wunused-variable\"") \
-        typeName _;\
-        _Pragma("clang diagnostic pop") \
-        [BIEnumsRegistry.defaultInstance registerEnum: self name:@metamacro_stringify(typeName)] \
+            _Pragma("clang diagnostic push") \
+            _Pragma("clang diagnostic ignored \"-Wunused-variable\"") \
+            typeName _;\
+            _Pragma("clang diagnostic pop") \
+            [BIEnumsRegistry.defaultInstance registerEnum: self name:@metamacro_stringify(typeName)] \
 
 #else
     #define BIRegisterSelfAsEnum(typeName) [BIEnumsRegistry.defaultInstance registerEnum:self name: @#typeName]
