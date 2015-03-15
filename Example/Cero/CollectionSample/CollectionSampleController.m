a96522e5b9a73435535ceeff7c5b8ee674a4f30f
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2015-03-15 11:53:58 +0100

    #10 Improve collection sample layout

--- Example/Cero/CollectionSample/CollectionSampleController.m
+++ Example/Cero/CollectionSample/CollectionSampleController.m
@@ -21,7 +21,7 @@
 
 - (void)loadView {
     [super loadView];
-    self.view.backgroundColor = [UIColor grayColor];
+    self.collectionView.backgroundColor = [UIColor clearColor];
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"CollectionSampleCell_0"];
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"CollectionSampleCell_1"];
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"CollectionSampleCell_2"];
@@ -29,24 +29,9 @@
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     NSString *layoutName = [NSString stringWithFormat:@"CollectionSampleCell_%@", @(indexPath.section % 3)];
-    __typeof(self) __weak bself = self;
-    return [_layoutLoader fillCollectionCellContent:collectionView layout:layoutName indexPath:indexPath loaded:^(NSObject <CollectionCellContent> *helper) {
-        __typeof(self) __strong self = bself;
-//        helper.logo.tintColor = [self colorForIndexPath:indexPath];
-        helper.root.backgroundColor = [self colorForIndexPath:indexPath];
+    UICollectionViewCell *cell = [_layoutLoader fillCollectionCellContent:collectionView layout:layoutName indexPath:indexPath loaded:^(NSObject <CollectionCellContent> *helper) {
     }];
-}
-
-- (UIColor *)colorForIndexPath:(NSIndexPath *)path {
-    NSInteger rowValue = path.row + 1;
-    NSInteger sectionValue = path.section + 1;
-    float rowMax = 5.f;
-    float sectionMax = 100.f;
-    UIColor *color = [UIColor colorWithRed:rowValue * 255 / rowMax
-                                     green:sectionValue * 255 / sectionMax
-                                      blue:(rowValue * sectionValue) * 255 / sectionMax * rowMax
-                                     alpha:1];
-    return color;
+    return cell;
 }
 
 
